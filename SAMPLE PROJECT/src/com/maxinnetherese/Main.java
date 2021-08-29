d4f0893e78f46e1eb96d45d8544072ba5dd569bd
Maxinne Tiu <maxinnetherese.tiu@benilde.edu.ph>
2021-08-29 14:41:15 +0800

    Added SAMPLE PROJECT

---
+++ SAMPLE PROJECT/src/com/maxinnetherese/Main.java
@@ -0,0 +1,19 @@
+package com.maxinnetherese;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //prints out hello world
+        System.out.println("Hello World :)))");
+
+        // gets user input
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello " + name);
+    }
+}
