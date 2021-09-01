496333e94966bcd727ae7ef61a10b4b8cd80f3cb
Maxinne Tiu <maxinnetherese.tiu@benilde.edu.ph>
2021-09-01 21:52:10 +0800

    added first answer for quiz 1

---
+++ gdstruc-module1-sorting/src/com/gdstruc/module1/Main.java
@@ -0,0 +1,72 @@
+package com.gdstruc.module1;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int[] numbers = new int[10];
+        numbers[0] = 25;
+        numbers[1] = 5;
+        numbers[2] = 23;
+        numbers[3] = 17;
+        numbers[4] = 90;
+        numbers[5] = 84;
+        numbers[6] = 24;
+        numbers[7] = 19;
+        numbers[8] = 58;
+        numbers[9] = 73;
+
+        System.out.println("numbers: ");
+        printArrayElements(numbers);
+
+        bubbleSort(numbers);
+        System.out.println("\n\nArranging using bubble sort in descending order: ");
+        printArrayElements(numbers);
+
+        selectionSort(numbers);
+        System.out.println("\n\nArranging using selection sort in ascending order: ");
+        printArrayElements(numbers);
+
+    }
+
+    private static void bubbleSort(int arr[])
+    {
+        for(int lastSortedIndex = arr.length-1;lastSortedIndex>0;lastSortedIndex--)
+        {
+            for(int i=0;i<lastSortedIndex;i++)
+            {
+                if(arr[i]<arr[i+1])
+                {
+                    int temp = arr[i+1];
+                    arr[i+1] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+    }
+
+    private static void selectionSort(int[] arr)
+    {
+        for(int lastSortedIndex = arr.length-1;lastSortedIndex>0;lastSortedIndex--)
+        {
+            int largestIndex = 0;
+            for(int i = 0; i<lastSortedIndex;i++)
+            {
+                if(arr[i]>arr[largestIndex])
+                {
+                    largestIndex = i;
+                }
+            }
+            int temp = arr[lastSortedIndex];
+            arr[lastSortedIndex] = arr[largestIndex];
+            arr[largestIndex] = temp;
+        }
+    }
+
+    private static void printArrayElements(int[] arr)
+    {
+        for (int j : arr) {
+            System.out.print(j + " ");
+        }
+    }
+
+}
