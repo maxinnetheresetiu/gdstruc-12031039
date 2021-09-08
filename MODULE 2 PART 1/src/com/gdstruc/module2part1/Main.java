49986b56e98ac1417c937c85e716bf98b62c5f73
Maxinne Tiu <maxinnetherese.tiu@benilde.edu.ph>
2021-09-08 23:07:09 +0800

    Added module 2 part 1

---
+++ MODULE 2 PART 1/src/com/gdstruc/module2part1/Main.java
@@ -0,0 +1,14 @@
+package com.gdstruc.module2part1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Player> playerList = new ArrayList<>();
+
+        playerList.add(new Player(1, "Asuna",))
+    }
+}
