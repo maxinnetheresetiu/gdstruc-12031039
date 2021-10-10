e604163ce2ade298fb6e6c109b99eeeb554b2c83
Maxinne Tiu <maxinnetherese.tiu@benilde.edu.ph>
2021-10-11 01:35:41 +0800

    started quiz 3

---
+++ Quiz 3/src/com/gdstruc/quiz3/ArrayQueue.java
@@ -0,0 +1,71 @@
+package com.gdstruc.quiz3;
+
+import java.util.NoSuchElementException;
+
+public class ArrayQueue {
+    private Player[] queue;
+    private int front;
+    private int back;
+
+    public ArrayQueue(int capacity)
+    {
+        queue = new Player[capacity];
+    }
+
+    public void enqueue(Player player)
+    {
+        // This resizes the array when it's full
+        if(back == queue.length)
+        {
+            Player[] newArray = new Player[queue.length * 2];
+            System.arraycopy(queue, 0, newArray, 0, queue.length);
+            queue = newArray;
+        }
+
+        queue[back] = player;
+        back++;
+    }
+
+    public Player dequeue()
+    {
+        if(size() == 0)
+        {
+            throw new NoSuchElementException();
+        }
+
+        Player removedPlayer = queue[front];
+        queue[front] = null;
+        front++;
+
+        if(size() == 0) // Reset trackers when queue is empty
+        {
+            front = 0;
+            back = 0;
+        }
+
+        return removedPlayer;
+    }
+
+    public Player peek()
+    {
+        if(size() == 0)
+        {
+            throw new NoSuchElementException();
+        }
+
+        return queue[front];
+    }
+
+    public int size()
+    {
+        return back - front;
+    }
+
+    public void printQueue()
+    {
+        for(int i = front; i < back; i++)
+        {
+            System.out.println(queue[i]);
+        }
+    }
+}
---
+++ Quiz 3/src/com/gdstruc/quiz3/Main.java
@@ -0,0 +1,9 @@
+package com.gdstruc.quiz3;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayQueue queue = new ArrayQueue(20);
+	    Player mochi = new Player(1, "mochi", 44);
+
+}
