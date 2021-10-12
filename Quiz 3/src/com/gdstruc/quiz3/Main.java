d06a7eed0971a23bf98b084a8ff5a9a4e549eca7
Maxinne Tiu <maxinnetherese.tiu@benilde.edu.ph>
2021-10-12 13:48:12 +0800

    Quiz 3 for checking

--- Quiz 3/src/com/gdstruc/quiz3/Main.java
+++ Quiz 3/src/com/gdstruc/quiz3/Main.java
@@ -1,9 +1,87 @@
 package com.gdstruc.quiz3;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
 
+    // Getting random number of players
+    public static int gettingNumOfPlayers()
+    {
+        Random rand = new Random();
+        int randomNumberOfPlayers = rand.nextInt(7);
+        randomNumberOfPlayers += 1;
+        return randomNumberOfPlayers;
+    }
+
     public static void main(String[] args) {
-        ArrayQueue queue = new ArrayQueue(20);
-	    Player mochi = new Player(1, "mochi", 44);
+        ArrayQueue playerQueue = new ArrayQueue(20);
+        playerQueue.enqueue(new Player(1, "judith"));
+        playerQueue.enqueue(new Player(2, "lucille"));
+        playerQueue.enqueue(new Player(3, "lilibeth"));
+        playerQueue.enqueue(new Player(4, "maryella"));
+        playerQueue.enqueue(new Player(5, "maura"));
+        playerQueue.enqueue(new Player(6, "millicent"));
+        playerQueue.enqueue(new Player(7, "prudence"));
+        playerQueue.enqueue(new Player(8, "quinnie"));
+        playerQueue.enqueue(new Player(9, "ruth"));
+        playerQueue.enqueue(new Player(10, "sophronia"));
+        playerQueue.enqueue(new Player(11, "llewellyn"));
+        playerQueue.enqueue(new Player(12,"milburn"));
+        playerQueue.enqueue(new Player(13, "neville"));
+        playerQueue.enqueue(new Player(14, "orson"));
+        playerQueue.enqueue(new Player(15, "saul"));
+        playerQueue.enqueue(new Player(16, "vaughn"));
+        playerQueue.enqueue(new Player(17, "wilmur"));
+        playerQueue.enqueue(new Player(18, "asa"));
+        playerQueue.enqueue(new Player(19, "brogan"));
+        playerQueue.enqueue(new Player(20, "clement"));
+
+        ArrayQueue queue = new ArrayQueue(15);
+
+        Scanner pause = new Scanner(System.in);
+
+        int game = 0;
+        while(game < 10)
+        {
+            System.out.println("Getting players...");
+            while(queue.size() < 5)
+            {
+                for (int i = 0; i < gettingNumOfPlayers(); i++)
+                {
+                    Player player = playerQueue.dequeue();
+                    queue.enqueue(player);
+                }
+
+                if(queue.size() < 5)
+                {
+                    System.out.println("There are " + queue.size() + " players in the queue. Getting another random number of players.");
+                   pause.nextLine();
+                }
+            }
+
+            System.out.println("Current queue:");
+            queue.printQueue();
+            pause.nextLine();
+
+            System.out.println("Starting the game!!");
+            int round = game + 1;
+            System.out.println("ROUND " + round);
+            pause.nextLine();
+            System.out.println("Popping 5 players from queue...");
+            for(int i = 0; i < 5; i++)
+            {
+                Player player = queue.dequeue();
+                playerQueue.enqueue(player);
+            }
+
+            System.out.println("Current queue:");
+            queue.printQueue();
+            pause.nextLine();
+
+            game++;
+        }
+        System.out.println("Game ended!!");
+    }
 
 }
--- Quiz 3/src/com/gdstruc/quiz3/Player.java
+++ Quiz 3/src/com/gdstruc/quiz3/Player.java
@@ -5,12 +5,10 @@ import java.util.Objects;
 public class Player {
     private int id;
     private String name;
-    private int level;
 
-    public Player(int id, String name, int level) {
+    public Player(int id, String name) {
         this.id = id;
         this.name = name;
-        this.level = level;
     }
 
     public int getId() { return id; }
@@ -21,16 +19,11 @@ public class Player {
 
     public void setName(String name) { this.name = name; }
 
-    public int getLevel() { return level; }
-
-    public void setLevel(int level) { this.level = level; }
-
     @Override
     public String toString() {
         return "Player{" +
                 "id=" + id +
-                ", name='" + name + '\'' +
-                ", level=" + level +
+                ", name='" + name +
                 '}';
     }
 
@@ -39,11 +32,11 @@ public class Player {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Player player = (Player) o;
-        return id == player.id && level == player.level && Objects.equals(name, player.name);
+        return id == player.id && Objects.equals(name, player.name);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, level);
+        return Objects.hash(id, name);
     }
 }
